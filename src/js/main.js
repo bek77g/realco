bf64079dcca24fac08ba1cdb45fbf3d79875d5a6
bek77g <d.zheenkul@gmail.com>
2023-04-15 19:09:46 +0600

    init: project

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+let burger = document.querySelector('.header__burger');
+let menu = document.querySelector('.header__menu');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  menu.classList.toggle('active');
+});
